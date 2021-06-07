506fa393b98378c3fd653491e69b1fe010b4309c
t4kuya <84721993+t4kuya@users.noreply.github.com>
2021-06-07 16:57:49 +0900

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -5,16 +5,15 @@ func Example_help() {
 	// Output:
 	// scv [OPTIONS] <VECTORS...>
 	// OPTIONS
-	//     -b   "Prints the number of bytes in each input file."
-	//
-        //     -m   "Prints the number of characters in each input file."
-	//
-        //     -l   "Prints the number of lines in each input file."
-	//
-        //     -w   "Prints the number of words in each input file."
-	//
-        //     -d   "Prints the creation date of file."
-	//
+	//     -a, --algorithm <ALGORITHM>    specifies the calculating algorithm.  This option is mandatory.
+	//                                    The value of this option accepts several values separated with comma.
+	//                                    Available values are: simpson, jaccard, dice, and cosine.
+	//     -f, --format <FORMAT>          specifies the resultant format. Default is default.
+	//                                    Available values are: default, json, and xml.
+	//     -t, --input-type <TYPE>        specifies the type of VECTORS. Default is file.
+	//                                    If TYPE is separated with comma, each type shows
+	//                                    the corresponding VECTORS.
+	//                                    Available values are: file, string, and json.
 	//     -h, --help                     prints this message.
 	// VECTORS
 	//     the source of vectors for calculation.
