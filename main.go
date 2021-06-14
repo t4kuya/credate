2355ebeeccbb3c70633025d0950d0c2112a749e0
t4kuya <aaa_1d_1115@icloud.com>
2021-06-14 19:11:07 +0900

    update

--- main.go
+++ main.go
@@ -9,18 +9,18 @@ import (
 func helpMessage(originalProgramName string) string {
 	name := filepath.Base(originalProgramName)
 	return fmt.Sprintf(`%s [OPTIONS] [FILEs...]
-OPTIONS
-     -b, --byte        Prints the number of bytes in each input file.
+	OPTIONS
+    -b, --byte        Prints the number of bytes in each input file.
 
-     -c, --character   Prints the number of characters in each input file.
+    -c, --character   Prints the number of characters in each input file.
 
-     -l, --line        Prints the number of lines in each input file.
+    -l, --line        Prints the number of lines in each input file.
 
-     -w, word          Prints the number of words in each input file.
+    -w, word          Prints the number of words in each input file.
 
-     -d, --date        Prints the creation date of file.
+    -d, --date        Prints the creation date of file.
 
-     -h, --help        Prints this message.
+    -h, --help        Prints this message.
 `, name)
 }
 
