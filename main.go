6057f8b0fc64c4e7c996355181f656463f33125f
t4kuya <aaa_1d_1115@icloud.com>
2021-06-14 17:59:26 +0900

    update

--- main.go
+++ main.go
@@ -8,20 +8,18 @@ import (
 
 func helpMessage(originalProgramName string) string {
 	name := filepath.Base(originalProgramName)
-	return fmt.Sprintf(`%s [OPTIONS] <VECTORS...>
+	return fmt.Sprintf(`%s [OPTIONS] [FILEs...]
 OPTIONS
-    -a, --algorithm <ALGORITHM>    specifies the calculating algorithm.  This option is mandatory.
-                                   The value of this option accepts several values separated with comma.
-                                   Available values are: simpson, jaccard, dice, and cosine.
-    -f, --format <FORMAT>          specifies the resultant format. Default is default.
-                                   Available values are: default, json, and xml.
-    -t, --input-type <TYPE>        specifies the type of VECTORS. Default is file.
-                                   If TYPE is separated with comma, each type shows
-                                   the corresponding VECTORS.
-                                   Available values are: file, string, and json.
-    -h, --help                     prints this message.
-VECTORS
-    the source of vectors for calculation.`, name)
+     -b, --byte        Prints the number of bytes in each input file.
+
+     -c, --character   Prints the number of characters in each input file.
+
+     -l, --line        Prints the number of lines in each input file.
+
+     -w, word          Prints the number of words in each input file.
+
+     -d, --date        Prints the creation date of file.
+`, name)
 }
 
 func goMain(args []string) int {
--- main_test.go
+++ main_test.go
@@ -3,18 +3,15 @@ package main
 func Example_help() {
 	goMain([]string{"/some/path/of/credate", "-h"})
 	// Output:
-	// credate [OPTIONS] <VECTORS...>
+	// credate [OPTIONS] [FILEs...]
 	// OPTIONS
-	//     -a, --algorithm <ALGORITHM>    specifies the calculating algorithm.  This option is mandatory.
-	//                                    The value of this option accepts several values separated with comma.
-	//                                    Available values are: simpson, jaccard, dice, and cosine.
-	//     -f, --format <FORMAT>          specifies the resultant format. Default is default.
-	//                                    Available values are: default, json, and xml.
-	//     -t, --input-type <TYPE>        specifies the type of VECTORS. Default is file.
-	//                                    If TYPE is separated with comma, each type shows
-	//                                    the corresponding VECTORS.
-	//                                    Available values are: file, string, and json.
-	//     -h, --help                     prints this message.
-	// VECTORS
-	//     the source of vectors for calculation.
+	//     -b, --byte        Prints the number of bytes in each input file.
+
+	//	   -c, --character   Prints the number of characters in each input file.
+
+	//     -l, --line        Prints the number of lines in each input file.
+
+	//     -w, word          Prints the number of words in each input file.
+
+	//     -d, --date        Prints the creation date of file.
 }
