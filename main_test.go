ea75ee387a1e27db58e113ad7030542ca9350de6
t4kuya <aaa_1d_1115@icloud.com>
2021-06-14 19:09:01 +0900

    update

--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ func Example_help() {
 	// OPTIONS
 	//     -b, --byte        Prints the number of bytes in each input file.
 	//
-	//	   -c, --character   Prints the number of characters in each input file.
+	//     -c, --character   Prints the number of characters in each input file.
 	//
 	//     -l, --line        Prints the number of lines in each input file.
 	//
