ebba5cd5b133dbb61bc596b50f50db2bbd73d08e
t4kuya <aaa_1d_1115@icloud.com>
2021-06-14 20:29:11 +0900

    update

---
+++ credate/credate.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	open := func(path string) []string {
+		f, err := os.Open(path)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "File %s could not read: %v\n", path, err)
+		}
+		defer f.Close()
+
+		lines := []string{}
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			lines = append(lines, scanner.Text())
+		}
+		if serr := scanner.Err(); serr != nil {
+			fmt.Fprintf(os.Stderr, "File %s scan error: %v\n", path, err)
+		}
+		return lines
+	}
+	file := open("../testdata/my_grandfather's_clock.txt")
+	fmt.Println(len(file))
+
+}
