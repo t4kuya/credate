5c3419382a2cfc2a66a8a45e87bfc6c481a5f2aa
t4kuya <aaa_1d_1115@icloud.com>
2021-06-18 15:04:18 +0900

    update

--- main.go
+++ main.go
@@ -11,15 +11,10 @@ func helpMessage(originalProgramName string) string {
 	return fmt.Sprintf(`%s [OPTIONS] [FILEs...]
 OPTIONS
     -b, --byte        Prints the number of bytes in each input file.
-
     -c, --character   Prints the number of characters in each input file.
-
     -l, --line        Prints the number of lines in each input file.
-
     -w, --word          Prints the number of words in each input file.
-
     -d, --date        Prints the creation date of file.
-
     -h, --help        Prints this message.
 `, name)
 }
--- main_test.go
+++ main_test.go
@@ -6,14 +6,9 @@ func Example_help() {
 	// credate [OPTIONS] [FILEs...]
 	// OPTIONS
 	//     -b, --byte        Prints the number of bytes in each input file.
-	//
 	//     -c, --character   Prints the number of characters in each input file.
-	//
 	//     -l, --line        Prints the number of lines in each input file.
-	//
 	//     -w, --word          Prints the number of words in each input file.
-	//
 	//     -d, --date        Prints the creation date of file.
-	//
 	//     -h, --help        Prints this message.
 }
