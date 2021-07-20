0ec7003eb0f87269dfca5dc25e6ded528922deb4
t4kuya <aaa_1d_1115@icloud.com>
2021-07-20 10:36:45 +0900

    update

---
+++ main_test.go
@@ -0,0 +1,14 @@
+package main
+
+func Example_help() {
+	goMain([]string{"/some/path/of/credate", "-h"})
+	// Output:
+	// credate [OPTIONS] [FILEs...]
+	//   OPTIONS
+	//     -b, --byte        Prints the number of bytes in each input file.
+	//     -c, --character   Prints the number of characters in each input file.
+	//     -l, --line        Prints the number of lines in each input file.
+	//     -w, --word        Prints the number of words in each input file.
+	//     -d, --date        Prints the creation date of file.
+	//     -h, --help        Prints this message.
+}
